6564b6cc9fdca4351a79aaafae6e2c9af9b24b58
Kelly Eggleton <kelly.eggleton@lwh.nhs.uk>
2015-11-10 05:41:46 -0800

    looping code for multiple analysis added

--- multi_inflammation_analysis.py
+++ multi_inflammation_analysis.py
@@ -1,26 +1,34 @@
 
 import numpy as np
 import matplotlib.pyplot as pp
+import glob
 
-data = np.loadtxt(fname='../data/inflammation-01.csv', delimiter=',')
+filenames = glob.glob('../data/inflammation*.csv')
+sliced_filenames = filenames [0:3]
 
-fig = pp.figure(figsize=(10.0, 3.0))
+for file in sliced_filenames:
 
-axes1 = fig.add_subplot(1,3,1)
-axes2 = fig.add_subplot(1,3,2)
-axes3 = fig.add_subplot(1,3,3)
 
-axes1.set_ylabel('Average')
-axes1.plot(data.mean(axis=0))
+	data = np.loadtxt(fname=file, delimiter=',')
 
-axes2.set_ylabel('Max')
-axes2.plot(data.max(axis=0))
+	fig = pp.figure(figsize=(10.0, 3.0))
 
-axes3.set_ylabel('Min')
-axes3.plot(data.min(axis=0))
+	axes1 = fig.add_subplot(1,3,1)
+	axes2 = fig.add_subplot(1,3,2)
+	axes3 = fig.add_subplot(1,3,3)
 
-fig.tight_layout()
+	axes1.set_ylabel('Average')
+	axes1.plot(data.mean(axis=0))
 
-pp.show()
+	axes2.set_ylabel('Max')
+	axes2.plot(data.max(axis=0))
+
+	axes3.set_ylabel('Min')
+	axes3.plot(data.min(axis=0))
+
+	fig.tight_layout()
+
+pp.show(fig)
+
 
 
